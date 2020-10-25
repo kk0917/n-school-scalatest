de072b46e1801353e08566e6cbad72aa567e9410
ko-kamenashi <kamenashi0917@gmail.com>
2020-10-25 12:51:27 +0900

    feat: add test cases

--- src/test/scala/CalcSpec.scala
+++ src/test/scala/CalcSpec.scala
@@ -1,16 +1,43 @@
 import org.scalatest._
+import org.scalatest.concurrent.TimeLimits._
+import org.scalatest.time.SpanSugar._
 
 class CalcSpec extends FlatSpec with DiagrammedAssertions {
   val calc = new Calc
 
-  "sum function" should "fetch integers array, and can return sum them all." in {
-    assert(calc.sum(Seq(1,2,3)) === 6)
-    assert(calc.sum(Seq(0)) === 0)
-    assert(calc.sum(Seq(-1,1)) === 0)
-    assert(calc.sum(Seq()) === 0)
+  "Calc.sum function" should "fetch integers array, and can return sum them all." in {
+    assert(calc.sum(Seq(1,2,3)) === 6) // true
+    assert(calc.sum(Seq(0)) === 0)     // true
+    assert(calc.sum(Seq(-1,1)) === 0)  // true
+//  assert(calc.sum(Seq()) === 9999)   // false
   }
 
   it should "Overflow occure when sum them all is over Integer.MAX_VALUE." in {
     assert(calc.sum(Seq(Integer.MAX_VALUE, 1)) === Integer.MIN_VALUE)
   }
+
+  "Calc.div function" should "Receive two Integer value, and return float value divided the numerator by the denominator." in {
+    assert(calc.div(6, 3) === 2.0)
+    assert(calc.div(1, 3) === 0.3333333333333333)
+  }
+  it should "Exception is thrown when dividing by 0." in {
+    intercept[ArithmeticException] {
+      calc.div(1, 0)
+    }
+  }
+
+  "Calc.isPrime function" should "Return boolean value whether or not it is prime number." in {
+    assert(calc.isPrime(0) === false)
+    assert(calc.isPrime(-1) === false)
+    assert(calc.isPrime(2))
+    assert(calc.isPrime(17))
+  }
+
+  it should "Be able to process judging prime number under 1000 thousand value within 1 seconds." in {
+    concurrent.TimeLimits.failAfter(1000 millis) {
+        assert(calc.isPrime(9999991))
+      }
+  }
+
+  // TODO: How to output asserts value?
 }
